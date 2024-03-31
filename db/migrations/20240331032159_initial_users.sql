fc2a7eb32fb3d712749bf03092bbb65b5ec435ac
TheRoniOne <ronidani99@hotmail.com>
2024-03-30 22:27:35 -0500

    Add initial users migration

---
+++ db/migrations/20240331032159_initial_users.sql
@@ -0,0 +1,4 @@
+-- Create "users" table
+CREATE TABLE `users` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `username` text NOT NULL, `salted_hash` text NOT NULL, `firstname` text NOT NULL, `lastname` text NOT NULL);
+-- Create index "idx_id" to table: "users"
+CREATE INDEX `idx_id` ON `users` (`id`);
