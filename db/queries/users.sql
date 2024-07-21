10b28b45fe6e728bb65b197e1c5bc701bcc32244
TheRoniOne <ronidani99@hotmail.com>
2024-07-20 22:08:29 -0500

    Add sqlc conf and queries

---
+++ db/queries/users.sql
@@ -0,0 +1,27 @@
+-- name: GetUser :one
+SELECT * FROM Users
+WHERE id = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM Users;
+
+-- name: CreateUser :one
+INSERT INTO Users (
+  username, email, salted_hash, firstname, lastname
+) VALUES (
+  $1, $2, $3, $4, $5
+)
+RETURNING *;
+
+-- name: UpdateUser :exec
+UPDATE Users
+  set username = $2,
+  email = $3,
+  salted_hash = $4,
+  firstname = $5,
+  lastname = $6
+WHERE id = $1;
+
+-- name: DeleteUser :exec
+DELETE FROM Users
+WHERE id = $1;
