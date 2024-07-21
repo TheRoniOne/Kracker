616d99f4d35c2f3cc7e8d361b1b5855afae411ba
TheRoniOne <ronidani99@hotmail.com>
2024-07-21 01:03:27 -0500

    Add params to ConnectToDB

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/TheRoniOne/Kracker/db/sqlc"
 	"github.com/TheRoniOne/Kracker/handlers"
 	"github.com/TheRoniOne/Kracker/initializers"
@@ -12,15 +14,14 @@ var DBPool *pgxpool.Pool
 
 func init() {
 	initializers.LoadEnvVars()
-	DBPool := initializers.ConnectToDB()
+	DBPool := initializers.ConnectToDB(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 }
 
 func main() {
 	defer DBPool.Close()
+
 	e := echo.New()
-
 	queries := sqlc.New(DBPool)
-
 	handlers.SetUpRoutes(e, queries)
 
 	e.Logger.Fatal(e.Start(":1323"))
--- initializers/db.go
+++ initializers/db.go
@@ -8,8 +8,9 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func ConnectToDB() *pgxpool.Pool {
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+func ConnectToDB(dbUser, dbPassword, dbHost, dbPort, dbName string) *pgxpool.Pool {
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 	DBPool, err := pgxpool.New(context.Background(), connStr)
 
 	if err != nil {
