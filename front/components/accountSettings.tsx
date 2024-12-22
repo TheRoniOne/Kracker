db77975406ca85fff1703b7dbc9cf7bb78b16724
TheRoniOne <ronidani99@gmail.com>
2024-12-22 16:59:07 -0500

    Fixes

--- front/components/accountSettings.tsx
+++ front/components/accountSettings.tsx
@@ -1,35 +1,33 @@
-"use client";
-
-import * as React from "react";
-
 import { Button } from "@/components/ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
-	DropdownMenuLabel,
 	DropdownMenuRadioGroup,
 	DropdownMenuRadioItem,
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
-export function DropdownMenuRadioGroupDemo() {
-	const [position, setPosition] = React.useState("bottom");
-
+export function AccountSettingsDropdown() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline">Open</Button>
+				<Button variant="outline">Account</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-56">
-				<DropdownMenuLabel>Panel Position</DropdownMenuLabel>
 				<DropdownMenuSeparator />
-				<DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-					<DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
-					<DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
-					<DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+				<DropdownMenuRadioGroup>
+					<DropdownMenuRadioItem value="settings">
+						<Link href="/settings">Settings</Link>
+					</DropdownMenuRadioItem>
+					<DropdownMenuRadioItem value="logout">
+						<Link href="/logout">Logout</Link>
+					</DropdownMenuRadioItem>
 				</DropdownMenuRadioGroup>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
 }
+
+export default AccountSettingsDropdown;
--- front/components/providers.tsx
+++ front/components/providers.tsx
@@ -3,6 +3,10 @@
 import { Provider } from "jotai";
 import type { ReactNode } from "react";
 
-export function Providers(children: ReactNode) {
+export function Providers({
+	children,
+}: Readonly<{
+	children: ReactNode;
+}>) {
 	return <Provider>{children}</Provider>;
 }
