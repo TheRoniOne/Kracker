87fbb36dee9779c5010d9d4f455ac4393b13495d
TheRoniOne <ronidani99@gmail.com>
2024-12-28 12:03:03 -0500

    Use CSRF middleware

--- back/internal/start_server.go
+++ back/internal/start_server.go
@@ -12,13 +12,15 @@ import (
 
 func StartServer(e *echo.Echo, address string) {
 	e.Debug = Debug
-	e.Use(middleware.RequestIDMiddleware())
-	e.Use(middleware.LoggingMiddleware())
+
+	e.Use(echomiddleware.CSRF())
 	e.Use(echomiddleware.Recover())
 	e.Use(echomiddleware.RateLimiter(echomiddleware.NewRateLimiterMemoryStore(rate.Limit(RateLimit))))
 	e.Use(echomiddleware.TimeoutWithConfig(echomiddleware.TimeoutConfig{
 		Timeout: 25 * time.Second,
 	}))
+	e.Use(middleware.RequestIDMiddleware())
+	e.Use(middleware.LoggingMiddleware())
 
 	go func() {
 		err := e.Start(address)
