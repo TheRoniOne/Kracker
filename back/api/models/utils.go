4c69f3e46be84dc07f03b13f22ed018220bad362
TheRoniOne <ronidani99@gmail.com>
2024-12-28 12:33:13 -0500

    Update GetUserID

--- back/api/models/utils.go
+++ back/api/models/utils.go
@@ -1,16 +1,9 @@
 package models
 
 import (
-	"github.com/TheRoniOne/Kracker/db/sqlc"
 	"github.com/labstack/echo/v4"
 )
 
-func GetUserID(_ echo.Context) int64 {
-	return 1 // TODO implement
-}
-
-func GetUserIDFromUser(u *sqlc.User) func(echo.Context) int64 {
-	return func(_ echo.Context) int64 {
-		return u.ID
-	}
+func GetUserID(c echo.Context) int64 {
+	return c.Get("UserID").(int64)
 }
