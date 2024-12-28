ad8c949ca05583837754e96067b6e189c66381be
TheRoniOne <ronidani99@gmail.com>
2024-12-28 18:15:27 -0500

    Remove bugged timeout middleware

--- back/internal/start_server.go
+++ back/internal/start_server.go
@@ -3,7 +3,6 @@ package internal
 import (
 	"log/slog"
 	"net/http"
-	"time"
 
 	"github.com/TheRoniOne/Kracker/middleware"
 	"github.com/labstack/echo/v4"
@@ -24,9 +23,7 @@ func StartServer(e *echo.Echo, address string, exit chan bool) {
 	}))
 	e.Use(echomiddleware.Recover())
 	e.Use(echomiddleware.RateLimiter(echomiddleware.NewRateLimiterMemoryStore(rate.Limit(RateLimit))))
-	e.Use(echomiddleware.TimeoutWithConfig(echomiddleware.TimeoutConfig{
-		Timeout: 25 * time.Second,
-	}))
+
 	e.Use(middleware.RequestIDMiddleware())
 	e.Use(middleware.LoggingMiddleware())
 
